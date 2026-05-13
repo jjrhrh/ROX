dc371a92d71fe956f82ef1e5ece13408a51b73aa
جواد <89310596+jjrhrh@users.noreply.github.com>
2026-05-13 20:13:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ HERO: Object.freeze({
     LIMIT         : 5,
     AUTOPLAY_MS   : 6500,
     TRANSITION_MS : 400,
-    POSTER_SIZE   : 'POSTER_MD',
+    POSTER_SIZE   : 'POSTER_XL',
     BACKDROP_SIZE : 'BACKDROP',
   }),
   // ─────────────────────────────────────────
