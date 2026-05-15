c32262072f71cd22ed7d3f8eb9ea1b2b612bae36
جواد <89310596+jjrhrh@users.noreply.github.com>
2026-05-15 12:51:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -397,8 +397,8 @@ document.body.style.backgroundImage = '';
     ? (movie.title || movie.original_title)
     : (movie.name  || movie.original_name);
   const poster = movie.poster_path
-    ? `${CONFIG.IMAGES.POSTER_SM}${movie.poster_path}`
-    : CONFIG.IMAGES.PLACEHOLDER;
+  ? `${CONFIG.IMAGES.POSTER_LG}${movie.poster_path}`
+  : CONFIG.IMAGES.PLACEHOLDER;
   const rating = movie.vote_average ? movie.vote_average.toFixed(1) : '';
   const year   = (movie.release_date || movie.first_air_date || '').slice(0,4);
   return `
